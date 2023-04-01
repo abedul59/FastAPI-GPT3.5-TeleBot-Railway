7f266420d6b52f9de8f512c7253a7d20ff95229c
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-01 10:42:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import httpx
 app = FastAPI()
+
+
+TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+
+client = httpx.AsyncClient()
+
 
 class Msg(BaseModel):
     msg: str
@@ -25,3 +32,13 @@ async def demo_post(inp: Msg):
 @app.get("/path/{path_id}")
 async def demo_get_path_id(path_id: int):
     return {"message": f"This is /path/{path_id} endpoint, use post request to retrieve result"}
+
+@app.post("/webhook/")
+async def webhook(req: Request):
+    data = await req.json()
+    chat_id = data['message']['chat']['id']
+    text = data['message']['text']
+
+    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={text}")
+
+    return data
