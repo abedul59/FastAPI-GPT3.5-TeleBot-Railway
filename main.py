64a9ef1029192e238b7aae7e5f3facf1b81f78be
abedul59 <70437442+abedul59@users.noreply.github.com>
2023-04-01 10:19:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,20 @@
-import os, uvicorn
-#import openai
-
-#import httpx
+import httpx
 from fastapi import FastAPI, Request
 
 
-#TOKEN = str(os.getenv("TELEGRAM_BOT_TOKEN"))
-#BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+TOKEN = "your_bot_token"
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
 
+client = httpx.AsyncClient()
 
-
-
-
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-#conversation = []
-'''
-class ChatGPT:  
-    
-
-    def __init__(self):
-        
-        self.messages = conversation
-        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
-
-
-
-    def get_response(self, user_input):
-        conversation.append({"role": "user", "content": user_input})
-        
-
-        response = openai.ChatCompletion.create(
-	            model=self.model,
-                messages = self.messages
-
-                )
-
-        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
-        
-        print("AI回答內容：")        
-        print(response['choices'][0]['message']['content'].strip())
-
-
-        
-        return response['choices'][0]['message']['content'].strip()
-	'''
-
-#client = httpx.AsyncClient()
 app = FastAPI()
-#chatgpt = ChatGPT()
 
-@app.get("/") # 指定 api 路徑 (get方法)
-async def hello():
-	return "Hello World from Flask in a uWSGI Nginx Docker container with \
-	     Python 3.8 (from the example template)"
-
-if __name__ == "__main__":
-	    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
-'''  
-@app.post("/callback")
-async def callback(req: Request):
+@app.post("/webhook/")
+async def webhook(req: Request):
     data = await req.json()
     chat_id = data['message']['chat']['id']
     text = data['message']['text']
-    ai_reply_response = chatgpt.get_response(text)  
 
-    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={ai_reply_response}")
+    await client.get(f"{BASE_URL}/sendMessage?chat_id={chat_id}&text={text}")
 
     return data
-    '''
